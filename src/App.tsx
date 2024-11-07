2224fc165ec354618033136ea8473705deb29648
Benedikt <bjdaurer@gmail.com>
2024-11-07 22:35:05 +0000

    playing with layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import './App.css'
 import Submission from './components/Submission';
 import Workflows from './components/Workflows';
 import { Visit } from 'workflows-lib';
+import { Grid } from '@mui/system';
+
 // import { WorkflowRelayFragment$key } from "./components/__generated__/WorkflowRelayFragment.graphql"
 // import { AppWorkflowsQuery as AppWorkflowsQueryType } from "./__generated__/AppWorkflowsQuery.graphql"
 // import { fetchQuery, graphql } from "relay-runtime";
@@ -54,15 +56,25 @@ function App() {
 
   return (
     <>
-      {/* <h1>Submit a Workflow</h1> */}
-      {/* <Templates/> */}
-      <Submission workflowName={workflowTemplateName} setVisit={setUserVisit}/>
-      <h2>List of workflows for {workflowTemplateName} in {JSON.stringify(userVisit)} </h2>
-      { userVisit !== undefined ? (
+
+    <Grid container spacing={20} columns={2}>
+      <Grid size={1} display="flex" justifyContent="center" alignItems="flex-start">
+        <Submission workflowName={workflowTemplateName} setVisit={setUserVisit}/>
+      </Grid>
+      <Grid size={1} display="flex" justifyContent="center" alignItems="flex-start">
+        <div>
+        <h2>List of workflows for {workflowTemplateName} in {JSON.stringify(userVisit)} </h2>
+        { userVisit !== undefined ? (
         <Suspense>
           <Workflows visit={userVisit}/>
         </Suspense>
           ) : ( <></> )}
+        </div>
+      </Grid>
+    </Grid>
+
+      {/* <h1>Submit a Workflow</h1> */}
+      {/* <Templates/> */}
     </>
   )
 }
